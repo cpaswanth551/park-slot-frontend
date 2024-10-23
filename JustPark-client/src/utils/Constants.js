253a3c81ced8240fcf4fe1b9410f40f526538ab0
cpaswanth551 <cpaswanth551@gmail.com>
2024-10-23 17:31:00 +0530

    signup url changed

--- JustPark-client/src/utils/Constants.js
+++ JustPark-client/src/utils/Constants.js
@@ -1,8 +1,8 @@
 // export const baseUrl = "http://127.0.0.1:8000/api/v1/";
 export const baseUrl = "https://kf-parking-app.vercel.app/api/v1/";
 
-export const userLogin = "auth/token/";
-export const userSignUP = "auth/create/";
+export const userLogin = "auth/token";
+export const userSignUP = "auth/create";
 export const getParkSlotbyId = "get-parkingSlotby-Id/";
 export const reservation_url = "reservations";
 export const create_Checkout_Session = "create-checkout-session/";
